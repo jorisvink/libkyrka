20635882aefef6a9fe861838bc72cc4bd3a0a282
Joris Vink <joris@coders.se>
2025-10-23 13:28:25 +0200

    dist-build: Add esp32s3 cleanup script.

---
+++ dist-build/esp32s3-clean.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Using the ESP-IDF toolchain compile libkyrka for an esp32s3 target.
+
+AR=xtensa-esp32s3-elf-ar
+CC=xtensa-esp32s3-elf-gcc
+
+DESTDIR=esp32s3 \
+    OSNAME=esp32 \
+    PREFIX= \
+    CROSS_BUILD=1 \
+    OBJDIR=obj-esp32s3 \
+    CC=$CC \
+    AR=$AR \
+    make clean
+
+rm -rf esp32s3
