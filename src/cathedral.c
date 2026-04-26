e8ae749c05060df2e88752216bd987be0eec41c8
Joris Vink <joris@sanctorum.se>
2026-04-26 20:48:33 +0200

    Give cathedral info flags to caller.

--- src/cathedral.c
+++ src/cathedral.c
@@ -437,7 +437,9 @@ cathedral_p2p_recv(struct kyrka *ctx, struct kyrka_offer *op)
 		return;
 
 	evt.type = KYRKA_EVENT_PEER_DISCOVERY;
+
 	evt.peer.ip = info->peer_ip;
+	evt.peer.flags = info->flags;
 	evt.peer.port = info->peer_port;
 
 	ctx->event(ctx, &evt, ctx->udata);
